5e5a325ad5bd83eeffb5229fa9bb2e165f294102
Garrett Burroughs <garrett@montclairrobotics.org>
2019-01-08 20:25:50 -0500

    Updated to 2019 WPILib with gradleRIO
    2019 implemented a new system for use of gradleRIO and vscode. This is the new updated base robot project for FRC

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,12 @@
+
+package frc.robot;
+
+import org.montclairrobotics.sprocket.SprocketRobot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends SprocketRobot {
+  
+}
